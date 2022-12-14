4de8d9b587bd7e1ff37edf39d804973a5295f0af
Martin Müller <tk@martinmueller.solutions>
2022-12-14 12:31:11 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'empty-nx-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'empty-nx-workspace';
+}
